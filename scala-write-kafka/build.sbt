8a0f8afffcfbe8d710b437a9b8e30e7d427cf779
root <root@dfadeyev-4.gce.cloudera.com>
2017-07-05 23:03:09 -0700

    scala-write-kafka

---
+++ scala-write-kafka/build.sbt
@@ -0,0 +1,9 @@
+import AssemblyKeys._
+assemblySettings
+name := "scala-write-kafka"
+version := "0.0.1"
+scalaVersion := "2.10.5"
+
+libraryDependencies ++= Seq(
+  "org.apache.kafka" % "kafka_2.10" % "0.10.2.0"
+)
---
+++ scala-write-kafka/src/main/scala/write-kafka.scala
@@ -0,0 +1,19 @@
+import java.util.Properties
+import org.apache.kafka.clients.producer._
+
+object WriteSocket {
+  def main(args: Array[String]) {
+    val  props = new Properties()
+    props.put("bootstrap.servers", "172.31.117.46:9092,172.31.117.47:9092,172.31.117.65:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](props)
+    val topic="test"
+    for(i<- 1 to 50){
+      val record = new ProducerRecord(topic, "key", s"hello $i")
+      producer.send(record)
+    }
+    producer.close()
+  }
+}
